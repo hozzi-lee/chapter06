0432cdd0eb31bfb97974174d5f3d4f5179cf24bd
leehojin-93 <hogenie93@gmail.com>
2021-06-18 13:03:40 +0900

    21. 06. 18. Fri) 오전수업 socketProgramming(Server, Client)

--- src/echo/ex01/Client.java
+++ src/echo/ex01/Client.java
@@ -1,8 +1,14 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
@@ -12,13 +18,54 @@ public class Client {
 
 		System.out.println("=== CLIENT START ===");
 		System.out.println("[Request ... a connection to the server ...]");
-//		System.out.println("... ... ... ... ...");
+		//		System.out.println("... ... ... ... ...");
 		System.out.println();
 
-		socket.connect( new InetSocketAddress( "192.168.35.135", 10001 ) );
+		socket.connect( new InetSocketAddress( "192.168.0.151", 10001 ) );
 		System.out.println("[Connected to server]");
 
+		// message OutputStream
+		//		OutputStream os = socket.getOutputStream();
+		//		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+		//		Scanner scan = new Scanner(System.in);
+		BufferedReader text = new BufferedReader(new InputStreamReader(System.in));
+
+		// message InputStream
+		//		InputStream is = socket.getInputStream();
+		//		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+		BufferedWriter sysout = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		while (true) {
+			// send message
+			// keyboard Input
+			//			String str = scan.nextLine();
+			String str = text.readLine();
+			if ("/q".equals(str)) {
+				System.out.println("Server out");
+				break;
+			}
+			bfw.write(str);
+			bfw.newLine();
+			bfw.flush();
+
+			// get message
+			String reM = bfr.readLine();
+			System.out.println("Message from server: " + reM);
+		}
+
+		//		System.out.println("=== CLIENT END ===");
+		sysout.write("=== CLIENT END ===");
+		sysout.newLine();
+		sysout.flush();
+
+		bfw.close();
+		text.close();
+		bfr.close();
+		//		scan.close();
 		socket.close();
+
 	}
 
 }
--- src/echo/ex01/Server.java
+++ src/echo/ex01/Server.java
@@ -1,6 +1,11 @@
 package echo.ex01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,16 +17,46 @@ public class Server {
 		ServerSocket serverSocket = new ServerSocket();
 
 		// ServerSocket bind
-		serverSocket.bind( new InetSocketAddress( "192.168.35.135", 10001 ) ); // ip, port
+		serverSocket.bind( new InetSocketAddress( "192.168.0.151", 10001 ) ); // ip, port
 
 		System.out.println("=== SERVER START ===");
 		System.out.println("[Waiting ... for a connection ...]");
-//		System.out.println("... ... ... ... ...");
+		//		System.out.println("... ... ... ... ...");
 		System.out.println();
 
 		Socket socket = serverSocket.accept();
 		System.out.println("[The client has been connected]");
 
+		// message InputStream
+		//		InputStream is = socket.getInputStream();
+		//		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+		// message OutputStream
+		//		OutputStream os = socket.getOutputStream();
+		//		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+
+		while (true) {
+			// get message
+			String msg = bfr.readLine();
+			if (msg == null) {
+				System.out.println("Client out");
+				break;
+			}
+			System.out.println("Message from client: " + msg);
+
+			// send message
+			bfw.write(msg);
+			bfw.newLine();
+			bfw.flush();
+		}
+
+		System.out.println("=== SERVER END ===");
+
+		bfr.close();
+		bfw.close();
+		socket.close();
 		serverSocket.close();
 
 	}
